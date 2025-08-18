16c46c6adf07ecd89b953932db9e08dcb2ce8ee9
elinaols <elol0031@student.hv.se>
2025-08-18 16:30:21 +0200

    Small fix for the router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Projects from './pages/projects/Projects.jsx'
 import Home from './pages/home/Home.jsx'
 import Cv from './pages/cv/Cv.jsx'
@@ -11,7 +11,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/elina-olsson-portfolio/'>
+    <Router>
       <Routes>
         <Route path="/" element={<App/>}>
           <Route index element={<Home/>}/>
@@ -19,6 +19,6 @@ createRoot(document.getElementById('root')).render(
           <Route path='/cv' element={<Cv/>}/>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   </StrictMode>
 );
